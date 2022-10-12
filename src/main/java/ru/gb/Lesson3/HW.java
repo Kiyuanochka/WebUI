82fe779813fe6124799c929c701c5964f9309b00
Kiyuanochka <93634306+Kiyuanochka@users.noreply.github.com>
2022-10-12 15:42:44 +0700

    Begining

---
+++ src/main/java/ru/gb/Lesson3/HW.java
@@ -0,0 +1,39 @@
+package ru.gb.Lesson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public class HW {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver webDriver = new ChromeDriver();
+        WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+
+
+        webDriver.get("https://gb.ru/");
+        webDriver.findElement(By.xpath("//a[text()='Поиск работы']")).click();
+
+        Thread.sleep(5000);
+
+        webDriver.get("https://gb.ru/");
+        webDriver.findElement(By.xpath("//div [contains(@class,'mn-header__icons')] //img [contains(@alt,'magnifier-icon')]")).click();
+        webDriver.findElement(By.xpath("//input [contains(@class,'mn-search-panel__input')]")).sendKeys("Карьера");
+
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button [contains(@class,'mn-search-panel__submit')]")));
+
+        webDriver.findElement(By.xpath("//button [contains(@class,'mn-search-panel__submit')]")).click();
+        Thread.sleep(5000);
+        webDriver.quit();
+
+
+    }
+}
