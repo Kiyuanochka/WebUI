5105eeba0d7a9769232556f474b32919dd5f58b8
Kiyuanochka <93634306+Kiyuanochka@users.noreply.github.com>
2022-11-24 09:51:08 +0700

    Замечаний с прошлой дз устранила так же

--- src/main/java/ru/gb/Lesson6/MainPage.java
+++ src/main/java/ru/gb/Lesson6/MainPage.java
@@ -1,13 +1,16 @@
 package ru.gb.Lesson6;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage extends BasePage{
 
-
-    @FindBy(xpath = "//div[@class=\"mn-header_hidden-mobile\"]/a[.='Войти']")
+    @FindBy(xpath = "//div[contains(@class,'mn-header_hidden-mobile')]/a[.='Войти']")
     private WebElement singInButton;
 
     public MainPage(WebDriver driver) {
@@ -19,7 +22,31 @@ public class MainPage extends BasePage{
         return new LoginPage(driver);
     }
 
+    @FindBy(xpath = "//div [contains(@class,'mn-header__icons')] //img [contains(@alt,'magnifier-icon')]")
+    private WebElement searchingButton;
+    @FindBy(xpath = "//input [contains(@class,'mn-search-panel__input')]")
+    private WebElement enterField;
+    @FindBy(xpath = "//button [contains(@class,'mn-search-panel__submit')]")
+    private WebElement enterButton;
 
+    public void clickSearchingButton(){
+        searchingButton.click();
+        enterField.sendKeys("Карьера");
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button [contains(@class,'mn-search-panel__submit')]")));
+        enterButton.click();
+    }
 
+    @FindBy(xpath = "//a [text()='Журнал']")
+    private WebElement postsButton;
 
-}
+    public void clickPostsButton(){
+        driver.manage().window().setSize(new Dimension(1366, 768));
+        postsButton.click();
+    }
+
+    @FindBy(xpath = "//div [contains(@class,'mn-trigger-menu mn-header_hidden-mobile')]")
+    private WebElement menuButton;
+
+    public  void clickMenuButton() throws InterruptedException {
+        menuButton.click();
+    }}
